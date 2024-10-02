1dd5c61ca243983d93133693a9a46c21f2da06b6
AvazxonAD <asqarovavazxon77@gmail.com>
2024-10-02 16:47:21 +0500

    jrunak

--- src/helpers/validation/spravochnik/organization.validation.js
+++ src/helpers/validation/spravochnik/organization.validation.js
@@ -3,10 +3,10 @@ const Joi = require("joi");
 const organizationValidation = Joi.object({
   name: Joi.string().required(),
   bank_klient: Joi.string().required(),
-  raschet_schet: Joi.string().required().length(20),
+  raschet_schet: Joi.string().required(),
   raschet_schet_gazna: Joi.string().required(),
   mfo: Joi.string().required(),
-  inn: Joi.string().length(9).required(),
+  inn: Joi.string().required(),
   okonx: Joi.string().required(),
 }).options({ stripUnknown: true });
 
