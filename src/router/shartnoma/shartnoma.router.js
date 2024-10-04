f6344a61d42f36bc0f578bdaddba924db2f0fc79
AvazxonAD <asqarovavazxon77@gmail.com>
2024-10-04 14:05:08 +0500

    jurnal

--- src/router/shartnoma/shartnoma.router.js
+++ src/router/shartnoma/shartnoma.router.js
@@ -18,6 +18,6 @@ router.get("/get/element/by/:id", protect, getElementById);
 router.put("/update/:id", protect, update_shartnoma);
 router.get("/get/by/organization/id/:id", protect, getByIdOrganization_Shartnoma);
 router.delete("/delete/:id", protect, deleteShartnoma);
-router.get('/for/jur/3', protect, forJur3)
+router.get('/for/jur3', protect, forJur3)
 
 module.exports = router;
