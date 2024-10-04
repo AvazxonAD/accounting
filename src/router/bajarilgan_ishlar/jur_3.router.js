82cc4a3111a03fb0c7c561a36e60d9705f4049db
AvazxonAD <asqarovavazxon77@gmail.com>
2024-10-04 14:24:38 +0500

    jurnal

--- src/router/bajarilgan_ishlar/jur_3.router.js
+++ src/router/bajarilgan_ishlar/jur_3.router.js
@@ -11,10 +11,10 @@ const {
   deleteJur_3,
 } = require("../../controller/bajarilgan_ishlar/jur_3.controller");
 
-router.post("/jur_3/create", protect, jur_3_create);
-router.get("/jur_3/get/all", protect, jur_3_get_all);
-router.put("/jur_3/update/:id", protect, jur_3_update);
-router.delete("/jur_3/delete/:id", protect, deleteJur_3);
-router.get("/get/element/by/id/jur_3/:id", protect, getElementByIdJur_3);
+router.post("/create", protect, jur_3_create);
+router.get("/get/all", protect, jur_3_get_all);
+router.put("/update/:id", protect, jur_3_update);
+router.delete("/delete/:id", protect, deleteJur_3);
+router.get("/get/element/by/:id", protect, getElementByIdJur_3);
 
 module.exports = router;
