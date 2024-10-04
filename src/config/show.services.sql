1763989a8f1e45fbb8fe322e2b8f5e7322ef9610
AvazxonAD <asqarovavazxon77@gmail.com>
2024-10-04 18:09:07 +0500

    jurnal

---
+++ src/config/show.services.sql
@@ -0,0 +1,32 @@
+CREATE TABLE kursatilgan_hizmatlar_jur152 (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
+    spravochnik_operatsii_own_id INT NOT NULL REFERENCES spravochnik_operatsii(id),
+    doc_num VARCHAR(255),
+    doc_date DATE,
+    summa DECIMAL,
+    opisanie VARCHAR(500),
+    id_spravochnik_organization INT NOT NULL REFERENCES spravochnik_organization(id),
+    shartnomalar_organization_id INT REFERENCES shartnomalar_organization(id),
+    main_schet_id INT NOT NULL REFERENCES main_schet(id),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    isdeleted BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE kursatilgan_hizmatlar_jur152_child (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
+    spravochnik_operatsii_id INT NOT NULL REFERENCES spravochnik_operatsii(id),
+    spravochnik_operatsii_own_id INT NOT NULL REFERENCES spravochnik_operatsii(id),
+    summa DECIMAL,
+    id_spravochnik_podrazdelenie INT REFERENCES spravochnik_podrazdelenie(id),
+    id_spravochnik_sostav INT REFERENCES spravochnik_sostav(id),
+    id_spravochnik_type_operatsii INT REFERENCES spravochnik_type_operatsii(id),
+    kursatilgan_hizmatlar_jur152_id INT NOT NULL REFERENCES kursatilgan_hizmatlar_jur152(id),
+    main_schet_id INT NOT NULL REFERENCES main_schet(id),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    isdeleted BOOLEAN DEFAULT FALSE
+);
+
---
+++ src/service/show services/show.services.service.js
@@ -0,0 +1,83 @@
+const pool = require('../../config/db')
+const { tashkentTime } = require('../../utils/date.function')
+
+const createService = async (data) => {
+    try {
+        const result = await pool.query(
+            `
+                INSERT INTO kursatilgan_hizmatlar_jur152(
+                    user_id,
+                    spravochnik_operatsii_own_id,
+                    doc_num,
+                    doc_date,
+                    summa,
+                    opisanie,
+                    id_spravochnik_organization,
+                    shartnomalar_organization_id,
+                    main_schet_id,
+                    created_at,
+                    updated_at
+                )
+                VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+            `, [
+            data.user_id,
+            data.spravochnik_operatsii_own_id,
+            data.doc_num,
+            data.doc_date,
+            data.summa,
+            data.opisanie,
+            data.id_spravochnik_organization,
+            data.shartnomalar_organization_id,
+            data.main_schet_id,
+            tashkentTime(),
+            tashkentTime()
+        ])
+        return result.rows[0]
+    } catch (error) {
+        console.log(`show services createService function:  ${error}`)
+        throw new Error(`show services createService function:  ${error.message}`)
+    }
+}
+
+const createChildService = async (data) => {
+    try {
+        await pool.query(
+            `
+                INSERT INTO kursatilgan_hizmatlar_jur152_child(
+                    user_id,
+                    spravochnik_operatsii_id,
+                    spravochnik_operatsii_own_id,
+                    summa,
+                    id_spravochnik_podrazdelenie,
+                    id_spravochnik_sostav,
+                    id_spravochnik_type_operatsii,
+                    kursatilgan_hizmatlar_jur152_id,
+                    main_schet_id,
+                    created_at,
+                    updated_at
+                )
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+            `, [
+                data.user_id,
+                data.spravochnik_operatsii_id,
+                data.spravochnik_operatsii_own_id,
+                data.summa,
+                data.id_spravochnik_podrazdelenie,
+                data.id_spravochnik_sostav,
+                data.id_spravochnik_type_operatsii,
+                data.kursatilgan_hizmatlar_jur152_id,
+                data.main_schet_id,
+                tashkentTime(),
+                tashkentTime()
+        ])
+    } catch (error) {
+        console.log(`show services createChildService function:  ${error}`)
+        throw new Error(`show services createChildService function:  ${error.message}`)
+    }
+}
+
+
+module.exports = {
+    createService,
+    createChildService
+}
