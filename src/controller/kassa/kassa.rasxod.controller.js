494acf12ba22bdcf35088723fc83a0feb6feb649
AvazxonAD <asqarovavazxon77@gmail.com>
2024-10-10 16:50:16 +0500

    jurnal

--- src/controller/kassa/kassa.rasxod.controller.js
+++ src/controller/kassa/kassa.rasxod.controller.js
@@ -1,198 +1,75 @@
-const asyncHandler = require("../../middleware/asyncHandler");
-const ErrorResponse = require("../../utils/errorResponse");
-const pool = require("../../config/db");
-const {
-  kassaValidation,
-} = require("../../helpers/validation/kassa/kassa.validation");
-const {
-  queryValidation,
-} = require("../../helpers/validation/bank/bank.prixod.validation");
-const { returnLocalDate } = require("../../utils/date.function");
-
-const { getByIdMainSchetService } = require("../../service/spravochnik/main.schet.service");
-const {
-  getByIdPodotchetService,
-} = require("../../service/spravochnik/podotchet.litso.service");
-const { getByIdOperatsiiService } = require("../../service/spravochnik/operatsii.service");
-const {
-  getByIdPodrazlanieService,
-} = require("../../service/spravochnik/podrazdelenie.service");
-const { getByIdSostavService } = require("../../service/spravochnik/sostav.service");
-const {
-  getByIdTypeOperatsiiService,
-} = require("../../service/spravochnik/type_operatsii.service");
 const {
   kassaRasxodCreateDB,
   kassaRasxodChild,
   getAllKassaRasxodDb,
-  getAllKassaRasxodChild,
   getElementById,
   updateKassaRasxodDB,
   deleteKassaRasxodChild,
   deleteKassaRasxodDB,
 } = require("../../service/kassa/kassa.rasxod.service");
-const { returnAllChildSumma, sum } = require("../../utils/returnSumma");
+const asyncHandler = require("../../middleware/asyncHandler");
+const ErrorResponse = require("../../utils/errorResponse");
+const { kassaValidation } = require("../../helpers/validation/kassa/kassa.validation");
+const { queryValidation } = require("../../helpers/validation/bank/bank.prixod.validation");
+const { getByIdMainSchetService } = require("../../service/spravochnik/main.schet.service");
+const { getByIdPodotchetService } = require("../../service/spravochnik/podotchet.litso.service");
+const { getByIdOperatsiiService } = require("../../service/spravochnik/operatsii.service");
+const { getByIdPodrazlanieService } = require("../../service/spravochnik/podrazdelenie.service");
+const { getByIdSostavService } = require("../../service/spravochnik/sostav.service");
+const { getByIdTypeOperatsiiService } = require("../../service/spravochnik/type_operatsii.service");
+const { returnAllChildSumma } = require("../../utils/returnSumma");
+const { validationResponse } = require("../../helpers/response-for-validation");
+const { resFunc } = require("../../helpers/resFunc");
 
 // kassa rasxod rasxod
 const kassaRasxodCreate = asyncHandler(async (req, res, next) => {
   const main_schet_id = req.query.main_schet_id;
   const user_id = req.user.id;
   const region_id = req.user.region_id;
-
-  const { error, value } = kassaValidation.validate(req.body);
-  if (error) {
-    return next(new ErrorResponse(error.details[0].message, 400));
+  const data = validationResponse(kassaValidation, req.body)
+  await getByIdMainSchetService(region_id, main_schet_id);
+  if (data.id_podotchet_litso) {
+    await getByIdPodotchetService(region_id, data.id_podotchet_litso);
   }
-
-  const main_schet = await getByIdMainSchetService(region_id, main_schet_id);
-  if (!main_schet) {
-    return next(new ErrorResponse("Server xatoli. Schet topilmadi", 404));
-  }
-
-  if (value.id_podotchet_litso) {
-    const podotchet_litso = await getByIdPodotchetService(
-      region_id,
-      value.id_podotchet_litso,
-    );
-    if (!podotchet_litso) {
-      return next(new ErrorResponse("podotchet_litso topilmadi", 404));
-    }
-  }
-
-  for (let child of value.childs) {
-    const spravochnik_operatsii = await getByIdOperatsiiService(
-      child.spravochnik_operatsii_id,
-      "kassa_rasxod",
-    );
-    if (!spravochnik_operatsii) {
-      return next(new ErrorResponse("spravochnik_operatsii topilmadi", 404));
-    }
-
+  for (let child of data.childs) {
+    await getByIdOperatsiiService(child.spravochnik_operatsii_id, "kassa_rasxod");
     if (child.id_spravochnik_podrazdelenie) {
-      const spravochnik_podrazdelenie = await getByIdPodrazlanieService(
-        region_id,
-        child.id_spravochnik_podrazdelenie,
-      );
-      if (!spravochnik_podrazdelenie) {
-        return next(
-          new ErrorResponse("spravochnik_podrazdelenie topilmadi", 404),
-        );
-      }
+      await getByIdPodrazlanieService(region_id, child.id_spravochnik_podrazdelenie);
     }
     if (child.id_spravochnik_sostav) {
-      const spravochnik_sostav = await getByIdSostavService(
-        region_id,
-        child.id_spravochnik_sostav,
-      );
-      if (!spravochnik_sostav) {
-        return next(new ErrorResponse("spravochnik_sostav topilmadi", 404));
-      }
+      await getByIdSostavService(region_id, child.id_spravochnik_sostav);
     }
     if (child.id_spravochnik_type_operatsii) {
-      const spravochnik_type_operatsii = await getByIdTypeOperatsiiService(
-        region_id,
-        child.id_spravochnik_type_operatsii,
-      );
-      if (!spravochnik_type_operatsii) {
-        return next(
-          new ErrorResponse("spravochnik_type_operatsii topilmadi", 404),
-        );
-      }
+      await getByIdTypeOperatsiiService(region_id, child.id_spravochnik_type_operatsii);
     }
   }
-  const summa = returnAllChildSumma(value.childs);
-
-  const kassa_rasxod = await kassaRasxodCreateDB({
-    ...value,
-    user_id,
-    main_schet_id,
-    summa,
-  });
-
-  for (let child of value.childs) {
-    await kassaRasxodChild({
-      ...child,
-      user_id,
-      main_schet_id,
-      kassa_rasxod_id: kassa_rasxod.id
-    });
+  const summa = returnAllChildSumma(data.childs);
+  const kassa_rasxod = await kassaRasxodCreateDB({ ...data, user_id, main_schet_id, summa });
+  const childs = []
+  for (let child of data.childs) {
+    const result = await kassaRasxodChild({ ...child, user_id, main_schet_id, kassa_rasxod_id: kassa_rasxod.id });
+    childs.push(result)
   }
-
-  res.status(201).json({
-    success: true,
-    data: "Muvaffaqiyatli kiritildi",
-  });
+  resFunc(res, 200, kassa_rasxod)
 });
 
 // get all kassa rasxod
 const getAllKassaRasxod = asyncHandler(async (req, res, next) => {
-  const { error, value } = queryValidation.validate(req.query);
-  if (error) {
-    return next(new ErrorResponse(error.details[0].message, 400));
-  }
-
-  const main_schet_id = value.main_schet_id;
+  const { main_schet_id, page, limit, from, to } = validationResponse(queryValidation, req.body)
   const region_id = req.user.region_id;
-
-  const limit = parseInt(value.limit) || 10;
-  const page = parseInt(value.page) || 1;
-  const from = value.from;
-  const to = value.to;
-
-  if (limit <= 0 || page <= 0) {
-    return next(
-      new ErrorResponse("Limit va page musbat sonlar bo'lishi kerak", 400),
-    );
-  }
-  const main_schet = await getByIdMainSchetService(region_id, main_schet_id);
-  if (!main_schet) {
-    return next(new ErrorResponse("Server xatoli. Schet topilmadi"));
-  }
-
+  await getByIdMainSchetService(region_id, main_schet_id);
   const offset = (page - 1) * limit;
-
-  const data = await getAllKassaRasxodDb(
-    region_id,
-    main_schet_id,
-    from,
-    to,
-    offset,
-    limit,
-  );
-
-  const resultArray = [];
-
-  for (let result of data.rows) {
-    const rasxod_child = await getAllKassaRasxodChild(
-      region_id,
-      main_schet_id,
-      result.id,
-    );
-    let object = { ...result };
-    object.summa = Number(object.summa);
-    object.childs = rasxod_child.map((item) => {
-      item.summa = Number(item.summa);
-      return item;
-    });
-    resultArray.push(object);
-  }
-
-  const total = Number(data.totalQuery.total_count);
+  const { data, total, summa } = await getAllKassaRasxodDb(region_id, main_schet_id, from, to, offset, limit);
   const pageCount = Math.ceil(total / limit);
-  const summa = Number(data.summa);
-
-  return res.status(200).json({
-    success: true,
-    meta: {
-      pageCount: pageCount,
-      count: total,
-      currentPage: page,
-      nextPage: page >= pageCount ? null : page + 1,
-      backPage: page === 1 ? null : page - 1,
-      summa,
-    },
-    data: resultArray,
-  });
+  const meta = {
+    pageCount: pageCount,
+    count: total,
+    currentPage: page,
+    nextPage: page >= pageCount ? null : page + 1,
+    backPage: page === 1 ? null : page - 1,
+    summa,
+  }
+  resFunc(res, 200, data, meta)
 });
 
 // kassa rasxod update
@@ -201,77 +78,25 @@ const updateKassaRasxodBank = asyncHandler(async (req, res, next) => {
   const region_id = req.user.region_id;
   const id = req.params.id;
   const user_id = req.user.id;
-
-  const result = await getElementById(region_id, main_schet_id, id);
-  if (!result) {
-    return next(
-      new ErrorResponse("Server xatolik. Rasxod document topilmadi", 404),
-    );
+  await getElementById(region_id, main_schet_id, id);
+  await getByIdMainSchetService(region_id, main_schet_id);
+  if (data.id_podotchet_litso) {
+    await getByIdPodotchetService(region_id, data.id_podotchet_litso);
   }
-
-  const { error, value } = kassaValidation.validate(req.body);
-  if (error) {
-    return next(new ErrorResponse(error.details[0].message, 400));
-  }
-
-  const main_schet = await getByIdMainSchetService(region_id, main_schet_id);
-  if (!main_schet) {
-    return next(new ErrorResponse("Server xatoli. Schet topilmadi", 404));
-  }
-  if (value.id_podotchet_litso) {
-    const podotchet_litso = await getByIdPodotchetService(
-      region_id,
-      value.id_podotchet_litso,
-    );
-    if (!podotchet_litso) {
-      return next(new ErrorResponse("podotchet_litso topilmadi", 404));
-    }
-  }
-
-  for (let child of value.childs) {
-    const spravochnik_operatsii = await getByIdOperatsiiService(
-      child.spravochnik_operatsii_id,
-      "kassa_rasxod",
-    );
-    if (!spravochnik_operatsii) {
-      return next(new ErrorResponse("spravochnik_operatsii topilmadi", 404));
-    }
-
+  for (let child of data.childs) {
+    await getByIdOperatsiiService(child.spravochnik_operatsii_id, "kassa_rasxod");
     if (child.id_spravochnik_podrazdelenie) {
-      const spravochnik_podrazdelenie = await getByIdPodrazlanieService(
-        region_id,
-        child.id_spravochnik_podrazdelenie,
-      );
-      if (!spravochnik_podrazdelenie) {
-        return next(
-          new ErrorResponse("spravochnik_podrazdelenie topilmadi", 404),
-        );
-      }
+      await getByIdPodrazlanieService(region_id, child.id_spravochnik_podrazdelenie);
     }
     if (child.id_spravochnik_sostav) {
-      const spravochnik_sostav = await getByIdSostavService(
-        region_id,
-        child.id_spravochnik_sostav,
-      );
-      if (!spravochnik_sostav) {
-        return next(new ErrorResponse("spravochnik_sostav topilmadi", 404));
-      }
+      await getByIdSostavService(region_id, child.id_spravochnik_sostav);
     }
     if (child.id_spravochnik_type_operatsii) {
-      const spravochnik_type_operatsii = await getByIdTypeOperatsiiService(
-        region_id,
-        child.id_spravochnik_type_operatsii,
-      );
-      if (!spravochnik_type_operatsii) {
-        return next(
-          new ErrorResponse("spravochnik_type_operatsii topilmadi", 404),
-        );
-      }
+      await getByIdTypeOperatsiiService(region_id, child.id_spravochnik_type_operatsii);
     }
   }
-  const summa = returnAllChildSumma(value.childs);
-
-  await updateKassaRasxodDB({ ...value, id, summa });
+  const summa = returnAllChildSumma(data.childs);
+  const rasxod = await updateKassaRasxodDB({ ...value, id, summa });
 
   await deleteKassaRasxodChild(id);
 
--- src/service/kassa/kassa.rasxod.service.js
+++ src/service/kassa/kassa.rasxod.service.js
@@ -1,24 +1,25 @@
 const pool = require("../../config/db");
-const { handleServiceError } = require("../../middleware/service.handle");
+const { tashkentTime } = require('../../utils/date.function');
+const ErrorResponse = require("../../utils/errorResponse");
 
-const kassaRasxodCreateDB = handleServiceError(async (data) => {
-  const result = await pool.query(
-    `
-            INSERT INTO kassa_rasxod(
-                doc_num, 
-                doc_date, 
-                opisanie, 
-                summa, 
-                id_podotchet_litso, 
-                main_schet_id, 
-                user_id,
-                created_at,
-                updated_at
-            ) 
-            VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) 
-            RETURNING * 
-        `,
-    [
+const kassaRasxodCreateDB = async (data) => {
+  try {
+    const result = await pool.query(
+      `
+        INSERT INTO kassa_rasxod(
+            doc_num, 
+            doc_date, 
+            opisanie, 
+            summa, 
+            id_podotchet_litso, 
+            main_schet_id, 
+            user_id,
+            created_at,
+            updated_at
+        ) 
+        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) 
+        RETURNING * 
+      `, [
       data.doc_num,
       data.doc_date,
       data.opisanie,
@@ -26,16 +27,19 @@ const kassaRasxodCreateDB = handleServiceError(async (data) => {
       data.id_podotchet_litso,
       data.main_schet_id,
       data.user_id,
-      new Date(),
-      new Date(),
-    ],
-  );
-  return result.rows[0];
-});
+      tashkentTime(),
+      tashkentTime(),
+    ]);
+    return result.rows[0];
+  } catch (error) {
+    throw new ErrorResponse(error, error.statusCode)
+  }
+}
 
-const kassaRasxodChild = handleServiceError(async (data) => {
-  await pool.query(
-    `
+const kassaRasxodChild = async (data) => {
+  try {
+    const result = await pool.query(
+      `
         INSERT INTO kassa_rasxod_child (
             spravochnik_operatsii_id,
             summa,
@@ -49,193 +53,197 @@ const kassaRasxodChild = handleServiceError(async (data) => {
             created_at, 
             updated_at
         )
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
-    `,
-    [
-      data.spravochnik_operatsii_id,
-      data.summa,
-      data.id_spravochnik_podrazdelenie,
-      data.id_spravochnik_sostav,
-      data.id_spravochnik_type_operatsii,
-      data.spravochnik_operatsii_own_id,
-      data.kassa_rasxod_id,
-      data.user_id,
-      data.main_schet_id,
-      new Date(),
-      new Date(),
-    ],
-  );
-});
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING * `,
+      [
+        data.spravochnik_operatsii_id,
+        data.summa,
+        data.id_spravochnik_podrazdelenie,
+        data.id_spravochnik_sostav,
+        data.id_spravochnik_type_operatsii,
+        data.spravochnik_operatsii_own_id,
+        data.kassa_rasxod_id,
+        data.user_id,
+        data.main_schet_id,
+        tashkentTime(),
+        tashkentTime(),
+      ]);
+    return result.rows[0]
+  } catch (error) {
+    throw new ErrorResponse(error, error.statusCode)
+  }
+}
 
-const getAllKassaRasxodChild = handleServiceError(
-  async (region_id, main_schet_id, kassa_rasxod_id) => {
-    const kassa_rasxod_child = await pool.query(
+const getAllKassaRasxodDb = async (region_id, main_schet_id, from, to, offset, limit) => {
+  try {
+    const result = await pool.query(
       `
-              SELECT  
-                  kassa_rasxod_child.id,
-                  kassa_rasxod_child.spravochnik_operatsii_id,
-                  kassa_rasxod_child.summa,
-                  kassa_rasxod_child.id_spravochnik_podrazdelenie,
-                  kassa_rasxod_child.id_spravochnik_sostav,
-                  kassa_rasxod_child.id_spravochnik_type_operatsii,
-                  kassa_rasxod_child.spravochnik_operatsii_own_id
-              FROM kassa_rasxod_child 
-              JOIN users ON users.id = kassa_rasxod_child.user_id
-              JOIN regions ON regions.id = users.region_id   
-              WHERE regions.id = $1 
-                AND kassa_rasxod_child.main_schet_id = $2 
-                AND kassa_rasxod_child.kassa_rasxod_id = $3
-          `,
-      [region_id, main_schet_id, kassa_rasxod_id],
-    );
-    return kassa_rasxod_child.rows;
-  },
-);
+        WITH data AS (
+              SELECT 
+                  k_r.id, 
+                  k_r.doc_num,
+                  TO_CHAR(k_r.doc_date, 'YYYY-MM-DD') AS doc_date, 
+                  k_r.opisanie, 
+                  k_r.summa, 
+                  k_r.id_podotchet_litso,
+                  s_p_l.name AS spravochnik_podotchet_litso_name,
+                  s_p_l.rayon AS spravochnik_podotchet_litso_rayon,
+                  (
+                      SELECT ARRAY_AGG(row_to_json(k_r_ch))
+                      FROM (
+                          SELECT  
+                              k_r_ch.id,
+                              k_r_ch.spravochnik_operatsii_id,
+                              k_r_ch.summa,
+                              k_r_ch.id_spravochnik_podrazdelenie,
+                              k_r_ch.id_spravochnik_sostav,
+                              k_r_ch.id_spravochnik_type_operatsii
+                          FROM kassa_rasxod_child AS k_r_ch 
+                          JOIN users AS u ON u.id = k_r.user_id
+                          JOIN regions AS r ON r.id = u.region_id   
+                          WHERE r.id = $1 
+                            AND k_r_ch.main_schet_id = $2 
+                            AND k_r_ch.kassa_prixod_id = k_r.id
+                      ) AS k_r_ch
+                  ) AS childs
+              FROM kassa_rasxod AS k_r
+              JOIN users AS u ON u.id = k_r.user_id
+              JOIN regions AS r ON r.id = u.region_id
+              JOIN spravochnik_podotchet_litso AS s_p_l ON s_p_l.id = k_r.id_podotchet_litso
+              WHERE r.id = $1 
+                AND k_r.main_schet_id = $2 
+                AND k_r.isdeleted = false 
+                AND k_r.doc_date BETWEEN $3 AND $4
+              OFFSET $5 LIMIT $6
+          )
+          SELECT 
+              ARRAY_AGG(row_to_json(data)) AS data,
+              (
+                  SELECT COALESCE(SUM(k_r.summa), 0)
+                  FROM kassa_rasxod AS k_r
+                  JOIN users AS u ON u.id = k_r.user_id
+                  JOIN regions AS r ON r.id = u.region_id  
+                  WHERE k_r.main_schet_id = $1 
+                    AND r.id = $2 
+                    AND k_r.doc_date BETWEEN $3 AND $4 
+                    AND k_r.isdeleted = false
+              )::FLOAT AS summa,
+              (
+                  SELECT COALESCE(COUNT(k_r.id), 0)
+                  FROM kassa_rasxod AS k_r
+                  JOIN users AS u ON u.id = k_r.user_id
+                  JOIN regions AS r ON r.id = u.region_id  
+                  WHERE r.id = $1 
+                    AND k_r.main_schet_id = $2 
+                    AND k_r.doc_date BETWEEN $3 AND $4 
+                    AND k_r.isdeleted = false
+              )::INTEGER AS total_count
+        FROM data
+      `, [main_schet_id, region_id, from, to, offset, limit]);
+    return { data: result.rows[0]?.data || [], summa: result.rows[0].summa, total: result.rows[0].total_count, };
+  } catch (error) {
+    throw new ErrorResponse(error, error.statusCode)
+  }
+}
 
-const getAllKassaRasxodDb = handleServiceError(
-  async (region_id, main_schet_id, from, to, offset, limit) => {
-    const results = await pool.query(
-      `   
-            SELECT 
-                kassa_rasxod.id, 
-                kassa_rasxod.doc_num,
-                TO_CHAR(kassa_rasxod.doc_date, 'YYYY-MM-DD') AS doc_date, 
-                kassa_rasxod.opisanie, 
-                kassa_rasxod.summa, 
-                kassa_rasxod.id_podotchet_litso,
-                spravochnik_podotchet_litso.name AS spravochnik_podotchet_litso_name,
-                spravochnik_podotchet_litso.rayon AS spravochnik_podotchet_litso_rayon
-            FROM kassa_rasxod
-            JOIN users ON users.id = kassa_rasxod.user_id
-            JOIN regions ON regions.id = users.region_id
-            JOIN spravochnik_podotchet_litso ON spravochnik_podotchet_litso.id = kassa_rasxod.id_podotchet_litso 
-            WHERE kassa_rasxod.main_schet_id = $1 
-                AND regions.id = $2
-                AND kassa_rasxod.isdeleted = false
-                AND kassa_rasxod.doc_date BETWEEN $3 AND $4
-                OFFSET $5
-                LIMIT $6
-        `,
-      [main_schet_id, region_id, from, to, offset, limit],
-    );
-
-    const summa = await pool.query(
-      `
-        SELECT 
-            COALESCE(SUM(kassa_rasxod.summa), 0) AS summa
-        FROM kassa_rasxod
-        JOIN users ON users.id = kassa_rasxod.user_id
-        JOIN regions ON regions.id = users.region_id  
-        WHERE kassa_rasxod.main_schet_id = $1 
-            AND regions.id = $2
-            AND kassa_rasxod.doc_date BETWEEN $3 AND $4
-    `,
-      [main_schet_id, region_id, from, to],
-    );
-
-    const total = await pool.query(
-      `
-        SELECT 
-            COALESCE(COUNT(kassa_rasxod.id), 0) AS total_count 
-        FROM kassa_rasxod
-        JOIN users ON users.id = kassa_rasxod.user_id
-        JOIN regions ON regions.id = users.region_id  
-        WHERE kassa_rasxod.main_schet_id = $1 
-            AND regions.id = $2
-            AND kassa_rasxod.doc_date BETWEEN $3 AND $4
-    `,
-      [main_schet_id, region_id, from, to],
-    );
-
-    return {
-      rows: results.rows,
-      summa: summa.rows[0].summa,
-      totalQuery: total.rows[0],
-    };
-  },
-);
-
-const getElementById = handleServiceError(
-  async (region_id, main_schet_id, id, ignoreDeleted = false) => {
-    let query = `
-      SELECT 
-          kassa_rasxod.id, 
-          kassa_rasxod.doc_num,
-          TO_CHAR(kassa_rasxod.doc_date, 'YYYY-MM-DD') AS doc_date, 
-          kassa_rasxod.opisanie, 
-          kassa_rasxod.summa, 
-          kassa_rasxod.id_podotchet_litso,
-          spravochnik_podotchet_litso.name AS spravochnik_podotchet_litso_name,
-          spravochnik_podotchet_litso.rayon AS spravochnik_podotchet_litso_rayon
-      FROM kassa_rasxod
-      JOIN users ON users.id = kassa_rasxod.user_id
-      JOIN regions ON regions.id = users.region_id
-      JOIN spravochnik_podotchet_litso ON spravochnik_podotchet_litso.id = kassa_rasxod.id_podotchet_litso  
-      WHERE kassa_rasxod.main_schet_id = $1 
-          AND regions.id = $2
-          AND kassa_rasxod.id = $3
-    `;
-
+const getElementById = async (region_id, main_schet_id, id, ignoreDeleted = false) => {
+  try {
+    let ignore = ``
     if (!ignoreDeleted) {
-      query += ` AND kassa_rasxod.isdeleted = false`;
+      ignore = `AND k_r.isdeleted = false`
     }
-
-    const result = await pool.query(query, [main_schet_id, region_id, id]);
+    const result = await pool.query(
+      ` SELECT 
+            k_r.id, 
+            k_r.doc_num,
+            TO_CHAR(k_r.doc_date, 'YYYY-MM-DD') AS doc_date, 
+            k_r.opisanie, 
+            k_r.summa::FLOAT, 
+            k_r.id_podotchet_litso,
+            s_p_l.name AS spravochnik_podotchet_litso_name,
+            s_p_l.rayon AS spravochnik_podotchet_litso_rayon,
+            (
+                SELECT ARRAY_AGG(row_to_json(k_r_ch))
+                FROM (
+                    SELECT  
+                        k_r_ch.id,
+                        k_r_ch.spravochnik_operatsii_id,
+                        k_r_ch.summa,
+                        k_r_ch.id_spravochnik_podrazdelenie,
+                        k_r_ch.id_spravochnik_sostav,
+                        k_r_ch.id_spravochnik_type_operatsii
+                    FROM kassa_rasxod_child AS k_r_ch 
+                    JOIN users AS u ON u.id = k_r.user_id
+                    JOIN regions AS r ON r.id = u.region_id   
+                    WHERE r.id = $1 
+                      AND k_r_ch.main_schet_id = $2 
+                      AND k_r_ch.kassa_prixod_id = k_r.id
+                ) AS k_r_ch
+            ) AS childs
+        FROM kassa_rasxod AS k_r
+        JOIN users AS u ON u.id = k_r.user_id
+        JOIN regions AS r ON r.id = u.region_id
+        JOIN spravochnik_podotchet_litso AS s_p_l ON s_p_l.id = k_r.id_podotchet_litso
+        WHERE r.id = $1 AND k_r.main_schet_id = $2 AND k_r.id = $3 ${ignore}
+      `, [main_schet_id, region_id, id]);
+    if (!result.rows[0]) {
+      throw new ErrorResponse('kassa prixod doc not found', 404)
+    }
     return result.rows[0];
-  },
-);
+  } catch (error) {
+    throw new ErrorResponse(error, error.statusCode)
+  }
+}
 
 
-const updateKassaRasxodDB = handleServiceError(async (data) => {
-  await pool.query(
-    `
-            UPDATE kassa_rasxod SET 
-                doc_num = $1, 
-                doc_date = $2, 
-                opisanie = $3, 
-                summa = $4, 
-                id_podotchet_litso = $5, 
-                updated_at = $6
-            WHERE id = $7
-        `,
-    [
-      data.doc_num,
-      data.doc_date,
-      data.opisanie,
-      data.summa,
-      data.id_podotchet_litso,
-      new Date(),
-      data.id,
-    ],
-  );
-});
+const updateKassaRasxodDB = async (data) => {
+  try {
+    const result = await pool.query(
+      `
+              UPDATE kassa_rasxod SET 
+                  doc_num = $1, 
+                  doc_date = $2, 
+                  opisanie = $3, 
+                  summa = $4, 
+                  id_podotchet_litso = $5, 
+                  updated_at = $6
+              WHERE id = $7 RETURNING *
+          `,
+      [
+        data.doc_num,
+        data.doc_date,
+        data.opisanie,
+        data.summa,
+        data.id_podotchet_litso,
+        tashkentTime(),
+        data.id,
+    ]);
+    return result.rows[0]
+  } catch (error) {
+    throw new ErrorResponse(error, error.statusCode)
+  }
+}
 
-const deleteKassaRasxodChild = handleServiceError(async (id) => {
-  await pool.query(
-    `DELETE FROM kassa_rasxod_child WHERE kassa_rasxod_id = $1`,
-    [id],
-  );
-});
+const deleteKassaRasxodChild = async (id) => {
+  try {
+    await pool.query(`DELETE FROM kassa_rasxod_child WHERE kassa_rasxod_id = $1`,[id]);
+  } catch (error) {
+    throw new ErrorResponse(error, error.statusCode)
+  }
+}
 
-const deleteKassaRasxodDB = handleServiceError(async (id) => {
-  await pool.query(
-    `UPDATE kassa_rasxod_child SET isdeleted = $1 WHERE kassa_rasxod_id = $2`,
-    [true, id],
-  );
-  await pool.query(
-    `UPDATE kassa_rasxod
-            SET isdeleted = true
-            WHERE id = $1 AND isdeleted = false
-        `,
-    [id],
-  );
-});
+const deleteKassaRasxodDB = async (id) => {
+  try {
+    await pool.query(`UPDATE kassa_rasxod_child SET isdeleted = $1 WHERE kassa_rasxod_id = $2`,[true, id]);
+    await pool.query(`UPDATE kassa_rasxod SET isdeleted = true WHERE id = $1 AND isdeleted = false`,[id],);
+  } catch (error) {
+    throw new ErrorResponse(error, error.statusCode)
+  }
+}
 
 module.exports = {
   kassaRasxodCreateDB,
   kassaRasxodChild,
   getAllKassaRasxodDb,
-  getAllKassaRasxodChild,
   getElementById,
   updateKassaRasxodDB,
   deleteKassaRasxodChild,
