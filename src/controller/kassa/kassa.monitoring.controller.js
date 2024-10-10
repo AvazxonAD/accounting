0e8f1efb5aed130e83a15bebad10bc3680d07050
AvazxonAD <asqarovavazxon77@gmail.com>
2024-10-10 18:00:40 +0500

    jurnal

--- src/controller/kassa/kassa.monitoring.controller.js
+++ src/controller/kassa/kassa.monitoring.controller.js
@@ -1,54 +1,22 @@
 const asyncHandler = require("../../middleware/asyncHandler");
 const ErrorResponse = require("../../utils/errorResponse");
 const { getAllMonitoring } = require("../../service/kassa/kassa.monitoring.service");
-const {
-  queryValidation,
-} = require("../../helpers/validation/bank/bank.prixod.validation");
+const { queryValidation } = require("../../helpers/validation/bank/bank.prixod.validation");
 const { getByIdMainSchetService } = require("../../service/spravochnik/main.schet.service");
+const { errorCatch } = require("../../helpers/errorCatch");
+const { validationResponse } = require("../../helpers/response-for-validation");
+const { resFunc } = require("../../helpers/resFunc");
 
-const getAllKassaMonitoring = asyncHandler(async (req, res, next) => {
-  const { error, value } = queryValidation.validate(req.query);
-  if (error) {
-    return next(new ErrorResponse(error.details[0].message, 400));
-  }
-  const region_id = req.user.region_id;
 
-  const limit = parseInt(value.limit) || 10;
-  const page = parseInt(value.page) || 1;
-  const main_schet_id = value.main_schet_id;
-  const from = value.from;
-  const to = value.to;
-
-  if (limit <= 0 || page <= 0) {
-    return next(
-      new ErrorResponse("Limit va page musbat sonlar bo'lishi kerak", 400),
-    );
-  }
-
-  const offset = (page - 1) * limit;
-  const main_schet = await getByIdMainSchetService(region_id, value.main_schet_id);
-  if (!main_schet) {
-    return next(new ErrorResponse("Schet topilmadi", 404));
-  }
-
-  const result = await getAllMonitoring(
-    region_id,
-    main_schet_id,
-    offset,
-    limit,
-    from,
-    to
-  );
-  
-  const total = Number(result.total_count);
-  const prixod_sum = Number(result.all_prixod_sum);
-  const rasxod_sum = Number(result.all_rasxod_sum);
-  const summaFrom = Number(result.total_sum_from);
-  const summaTo = Number(result.total_sum_to);
-  const pageCount = Math.ceil(total / limit);
-  return res.status(200).json({
-    success: true,
-    meta: {
+const getAllKassaMonitoring = async (req, res, next) => {
+  try {
+    const region_id = req.user.region_id
+    const { page, limit, main_schet_id, from, to } = validationResponse(queryValidation, req.query)
+    const offset = (page - 1) * limit;
+    await getByIdMainSchetService(region_id, main_schet_id);
+    const {total, data, summa_from, summa_to, prixod_sum, rasxod_sum } = await getAllMonitoring(region_id, main_schet_id, offset, limit, from, to);
+    const pageCount = Math.ceil(total / limit);
+    const meta = {
       pageCount: pageCount,
       count: total,
       currentPage: page,
@@ -56,12 +24,14 @@ const getAllKassaMonitoring = asyncHandler(async (req, res, next) => {
       backPage: page === 1 ? null : page - 1,
       prixod_sum,
       rasxod_sum,
-      summaFrom,
-      summaTo,
-    },
-    data: result.result_data,
-  });
-});
+      summa_from,
+      summa_to,
+    }
+    resFunc(res, 200, data, meta)
+  } catch (error) {
+    errorCatch(error, res)
+  }
+}
 
 module.exports = {
   getAllKassaMonitoring,
--- src/service/kassa/kassa.monitoring.service.js
+++ src/service/kassa/kassa.monitoring.service.js
@@ -1,148 +1,113 @@
 const pool = require("../../config/db");
-const { handleServiceError } = require("../../middleware/service.handle");
-const { returnLocalDate } = require("../../utils/date.function");
+const ErrorResponse = require("../../utils/errorResponse");
+const getAllMonitoring = async (region_id, main_schet_id, offset, limit, from, to) => {
+  try {
+    const { rows } = await pool.query(
+      ` 
+        WITH data AS (
+          SELECT 
+              kp.id, 
+              kp.doc_num,
+              TO_CHAR(kp.doc_date, 'YYYY-MM-DD') AS doc_date,
+              kp.summa AS prixod_sum,
+              0 AS rasxod_sum,
+              kp.id_podotchet_litso,
+              spravochnik_podotchet_litso.name AS spravochnik_podotchet_litso_name,
+              kp.opisanie,
+              kp.doc_date AS combined_date
+          FROM kassa_prixod kp
+          JOIN users u ON kp.user_id = u.id
+          JOIN regions r ON u.region_id = r.id
+          JOIN spravochnik_podotchet_litso ON spravochnik_podotchet_litso.id = kp.id_podotchet_litso
+          WHERE r.id = $1 AND kp.main_schet_id = $2
+          AND kp.doc_date BETWEEN $3 AND $4
 
-const getAllMonitoring = handleServiceError(
-  async (region_id, main_schet_id, offset, limit, from, to) => {
-    const data = await pool.query(
-      `
-        SELECT 
-            kp.id, 
-            kp.doc_num,
-            TO_CHAR(kp.doc_date, 'YYYY-MM-DD') AS doc_date,
-            kp.summa AS prixod_sum,
-            0 AS rasxod_sum,
-            kp.id_podotchet_litso,
-            spravochnik_podotchet_litso.name AS spravochnik_podotchet_litso_name,
-            kp.opisanie,
-            kp.doc_date AS combined_date
-        FROM kassa_prixod kp
-        JOIN users u ON kp.user_id = u.id
-        JOIN regions r ON u.region_id = r.id
-        JOIN spravochnik_podotchet_litso ON spravochnik_podotchet_litso.id = kp.id_podotchet_litso
-        WHERE r.id = $1 AND kp.main_schet_id = $2
-        AND kp.doc_date BETWEEN $3 AND $4
-        
-        UNION ALL
-        
-        SELECT 
-            kr.id, 
-            kr.doc_num,
-            TO_CHAR(kr.doc_date, 'YYYY-MM-DD') AS doc_date,
-            0 AS prixod_sum,
-            kr.summa AS rasxod_sum,
-            kr.id_podotchet_litso,
-            spravochnik_podotchet_litso.name,
-            kr.opisanie,
-            kr.doc_date AS combined_date
-        FROM kassa_rasxod kr
-        JOIN users u ON kr.user_id = u.id
-        JOIN regions r ON u.region_id = r.id
-        JOIN spravochnik_podotchet_litso ON spravochnik_podotchet_litso.id = kr.id_podotchet_litso
-        WHERE r.id = $1 AND kr.main_schet_id = $2
-        AND kr.doc_date BETWEEN $3 AND $4
-        ORDER BY combined_date
-        OFFSET $5 LIMIT $6;
-    `,
-      [region_id, main_schet_id, from, to, offset, limit],
-    );
+          UNION ALL
 
-    const total = await pool.query(
-      `
-        SELECT 
-        COALESCE((SELECT COUNT(kr.id) 
-                FROM kassa_rasxod kr
-                JOIN users u ON kr.user_id = u.id
-                JOIN regions r ON u.region_id = r.id
-                WHERE r.id = $1 AND kr.main_schet_id = $2 
-                AND kr.doc_date BETWEEN $3 AND $4), 0) +
-        COALESCE((SELECT COUNT(kp.id) 
-                FROM kassa_prixod kp
-                JOIN users u ON kp.user_id = u.id
-                JOIN regions r ON u.region_id = r.id
-                WHERE r.id = $1 AND kp.main_schet_id = $2 
-                AND kp.doc_date BETWEEN $3 AND $4), 0) AS total_count
-    `,
-      [region_id, main_schet_id, from, to],
-    );
-
-    const prixod_sum = await pool.query(
-      `
-        SELECT SUM(kp.summa) AS all_prixod_sum
-        FROM kassa_prixod kp
-        JOIN users u ON kp.user_id = u.id
-        JOIN regions r ON u.region_id = r.id
-        WHERE r.id = $1 AND kp.main_schet_id = $2 
-        AND kp.doc_date BETWEEN $3 AND $4
-    `,
-      [region_id, main_schet_id, from, to],
-    );
-
-    const rasxod_sum = await pool.query(
-      `
-        SELECT SUM(kr.summa) AS all_rasxod_sum
-        FROM kassa_rasxod kr
-        JOIN users u ON kr.user_id = u.id
-        JOIN regions r ON u.region_id = r.id
-        WHERE r.id = $1 AND kr.main_schet_id = $2 
-        AND kr.doc_date BETWEEN $3 AND $4
-    `,
-      [region_id, main_schet_id, from, to],
-    );
-
-    const summaFrom = await pool.query(`
+          SELECT 
+              kr.id, 
+              kr.doc_num,
+              TO_CHAR(kr.doc_date, 'YYYY-MM-DD') AS doc_date,
+              0 AS prixod_sum,
+              kr.summa AS rasxod_sum,
+              kr.id_podotchet_litso,
+              spravochnik_podotchet_litso.name,
+              kr.opisanie,
+              kr.doc_date AS combined_date
+          FROM kassa_rasxod kr
+          JOIN users u ON kr.user_id = u.id
+          JOIN regions r ON u.region_id = r.id
+          JOIN spravochnik_podotchet_litso ON spravochnik_podotchet_litso.id = kr.id_podotchet_litso
+          WHERE r.id = $1 AND kr.main_schet_id = $2
+          AND kr.doc_date BETWEEN $3 AND $4
+          ORDER BY combined_date
+          OFFSET $5 LIMIT $6
+      ) 
       SELECT 
-        COALESCE((SELECT SUM(kp.summa) 
-                FROM kassa_prixod kp
-                JOIN users u ON kp.user_id = u.id
-                JOIN regions r ON u.region_id = r.id
-                WHERE r.id = $1 AND kp.main_schet_id = $2 
-                AND kp.doc_date < $3), 0) -
-        COALESCE((SELECT SUM(kr.summa) 
-                FROM kassa_rasxod kr
-                JOIN users u ON kr.user_id = u.id
-                JOIN regions r ON u.region_id = r.id
-                WHERE r.id = $1 AND kr.main_schet_id = $2 
-                AND kr.doc_date < $3 ), 0) AS total_sum
-    `, [ region_id, main_schet_id, from]);
-    
-    const summaTo = await pool.query(`
-      SELECT 
-        COALESCE((SELECT SUM(kp.summa) 
-                FROM kassa_prixod kp
-                JOIN users u ON kp.user_id = u.id
-                JOIN regions r ON u.region_id = r.id
-                WHERE r.id = $1 AND kp.main_schet_id = $2 
-                AND kp.doc_date <= $3), 0) -
-        COALESCE((SELECT SUM(kr.summa) 
-                FROM kassa_rasxod kr
-                JOIN users u ON kr.user_id = u.id
-                JOIN regions r ON u.region_id = r.id
-                WHERE r.id = $1 AND kr.main_schet_id = $2 
-                AND kr.doc_date <= $3 ), 0) AS total_sum
-    `, [region_id, main_schet_id, to]);
-
-    const result_data = data.rows.map((row) => ({
-      id: row.id,
-      doc_num: row.doc_num,
-      doc_date: row.doc_date,
-      prixod_sum: Number(row.prixod_sum),
-      rasxod_sum: Number(row.rasxod_sum),
-      id_podotchet_litso: row.id_podotchet_litso,
-      spravochnik_podotchet_litso_name: row.spravochnik_podotchet_litso_name,
-      opisanie: row.opisanie,
-    }));
-
+          ARRAY_AGG(row_to_json(data)) AS data,
+          (SELECT 
+              COALESCE((SELECT COUNT(kr.id) 
+                        FROM kassa_rasxod kr
+                        JOIN users u ON kr.user_id = u.id
+                        JOIN regions r ON u.region_id = r.id
+                        WHERE r.id = $1 AND kr.main_schet_id = $2 
+                        AND kr.doc_date BETWEEN $3 AND $4), 0) +
+              COALESCE((SELECT COUNT(kp.id) 
+                        FROM kassa_prixod kp
+                        JOIN users u ON kp.user_id = u.id
+                        JOIN regions r ON u.region_id = r.id
+                        WHERE r.id = $1 AND kp.main_schet_id = $2 
+                        AND kp.doc_date BETWEEN $3 AND $4), 0))::INTEGER AS total_count,
+          (COALESCE((SELECT SUM(kp.summa)
+                    FROM kassa_prixod kp
+                    JOIN users u ON kp.user_id = u.id
+                    JOIN regions r ON u.region_id = r.id
+                    WHERE r.id = $1 AND kp.main_schet_id = $2 
+                    AND kp.doc_date BETWEEN $3 AND $4), 0))::FLOAT AS prixod_sum,
+          (COALESCE((SELECT SUM(kr.summa)
+                    FROM kassa_rasxod kr
+                    JOIN users u ON kr.user_id = u.id
+                    JOIN regions r ON u.region_id = r.id
+                    WHERE r.id = $1 AND kr.main_schet_id = $2 
+                    AND kr.doc_date BETWEEN $3 AND $4), 0))::FLOAT  AS rasxod_sum,
+          (SELECT COALESCE((SELECT SUM(kp.summa) 
+                            FROM kassa_prixod kp
+                            JOIN users u ON kp.user_id = u.id
+                            JOIN regions r ON u.region_id = r.id
+                            WHERE r.id = $1 AND kp.main_schet_id = $2 
+                            AND kp.doc_date < $3), 0) -
+                  COALESCE((SELECT SUM(kr.summa) 
+                            FROM kassa_rasxod kr
+                            JOIN users u ON kr.user_id = u.id
+                            JOIN regions r ON u.region_id = r.id
+                            WHERE r.id = $1 AND kr.main_schet_id = $2 
+                            AND kr.doc_date < $3), 0))::FLOAT  AS summa_from,
+          (SELECT COALESCE((SELECT SUM(kp.summa) 
+                            FROM kassa_prixod kp
+                            JOIN users u ON kp.user_id = u.id
+                            JOIN regions r ON u.region_id = r.id
+                            WHERE r.id = $1 AND kp.main_schet_id = $2 
+                            AND kp.doc_date <= $3), 0) -
+                  COALESCE((SELECT SUM(kr.summa) 
+                            FROM kassa_rasxod kr
+                            JOIN users u ON kr.user_id = u.id
+                            JOIN regions r ON u.region_id = r.id
+                            WHERE r.id = $1 AND kr.main_schet_id = $2 
+                            AND kr.doc_date <= $3), 0))::FLOAT  AS summa_to
+      FROM data`
+    , [region_id, main_schet_id, from, to, offset, limit])
     return {
-      result_data,
-      total_count: total.rows[0].total_count,
-      all_prixod_sum: prixod_sum.rows[0].all_prixod_sum,
-      all_rasxod_sum: rasxod_sum.rows[0].all_rasxod_sum,
-      total_sum_from: summaFrom.rows[0].total_sum,
-      total_sum_to: summaTo.rows[0].total_sum,
+      data: rows[0]?.data || [],
+      total: rows[0].total_count,
+      prixod_sum: rows[0].prixod_sum,
+      rasxod_sum: rows[0].rasxod_sum,
+      summa_from: rows[0].summa_from,
+      summa_to: rows[0].summa_to,
     };
-  },
-);
+  } catch (error) {
+    throw new ErrorResponse(error, error.statusCode)
+  }
+}
 
 module.exports = {
   getAllMonitoring,
