869fa52940ef391dd068769bba0115be8d2891dd
AvazxonAD <asqarovavazxon77@gmail.com>
2024-10-14 14:59:46 +0500

    jrunal

--- src/router/jur3/jur3.routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const { createJurnal3 } = require('../../controller/jur3/jurnal3.controller');
-const { protect } = require('../../middleware/auth');
-
-router.post('/', createJurnal3)
-
-module.exports = router;
