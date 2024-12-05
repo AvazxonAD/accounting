3d565058f7f176386a43fe98b6b146f11e9b7d17
AvazxonAD <asqarovavazxon77@gmail.com>
2024-12-05 21:55:16 +0500

    jurnal

--- src/kassa/kassa.prixod.controller.js
+++ src/kassa/kassa.prixod.controller.js
@@ -20,6 +20,7 @@ const { returnAllChildSumma } = require("../utils/returnSumma");
 const { validationResponse } = require("../utils/response-for-validation");
 const { resFunc } = require("../utils/resFunc");
 const { errorCatch } = require("../utils/errorCatch");
+const ErrorResponse = require('../utils/errorResponse')
 
 // kassa prixod rasxod
 const kassaPrixodCreate = async (req, res) => {
