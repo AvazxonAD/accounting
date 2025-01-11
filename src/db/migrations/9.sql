b1c06fdcf1e8e728f4ae29d905662b11d5a172e2
AvazxonAD <asqarovavazxon77@gmail.com>
2025-01-11 15:36:12 +0500

    jurnal

--- src/db/migrations/9.sql
+++ src/db/migrations/9.sql
@@ -1,5 +1,5 @@
 ALTER TABLE iznos_tovar_jur7 ADD COLUMN kimni_buynida DECIMAL;
 
-UPDATE iznos_tovar_jur7 SET kimni_buynida = 5;
+UPDATE iznos_tovar_jur7 SET kimni_buynida = (SELECT id FROM spravochnik_javobgar_shaxs_jur7 WHERE isdeleted = false LIMIT 1);
 
 ALTER TABLE iznos_tovar_jur7 ALTER COLUMN kimni_buynida SET NOT NULL;
