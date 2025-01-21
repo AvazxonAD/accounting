ab191471e16af8733768b3eb3d082f753b0794b1
AvazxonAD <asqarovavazxon77@gmail.com>
2025-01-22 02:09:08 +0500

    jurnal

--- src/akt/akt.routes.js
+++ src/akt/akt.routes.js
@@ -13,10 +13,10 @@ const {
 } = require("./akt.controller");
 
 router.post("/", jur_3_create);
+router.get('/export/cap', jur3Cap)
 router.get("/", jur_3_get_all);
 router.put("/:id", jur_3_update);
 router.delete("/:id", deleteJur_3);
-router.get('/export/cap', jur3Cap)
 router.get("/:id", getElementByIdJur_3);
 
 
