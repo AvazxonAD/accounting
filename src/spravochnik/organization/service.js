3425af3f88af3be51929ab2b6a463af1006f5747
AvazxonAD <asqarovavazxon77@gmail.com>
2025-02-06 01:20:18 +0500

    jurnal

--- src/spravochnik/organization/service.js
+++ src/spravochnik/organization/service.js
@@ -4,13 +4,13 @@ const { tashkentTime } = require('../../helper/functions')
 exports.OrganizationService = class {
     static async getByInn(data) {
         const result = await OrganizationDB.getByInn([data.region_id, data.inn]);
-   
+
         return result;
     }
 
     static async getByName(data) {
         const result = await OrganizationDB.getByName([data.region_id, data.name]);
-   
+
         return result;
     }
 
@@ -35,7 +35,7 @@ exports.OrganizationService = class {
     static async update(data) {
         const result = await OrganizationDB.update([
             data.name, data.bank_klient, data.raschet_schet,
-            data.raschet_schet_gazna, data.mfo, data.inn, data.okonx, 
+            data.raschet_schet_gazna, data.mfo, data.inn, data.okonx,
             data.parent_id, data.id
         ]);
 
@@ -44,15 +44,39 @@ exports.OrganizationService = class {
 
     static async delete(data) {
         const result = await OrganizationDB.delete([data.id]);
-        
+
         return result;
     }
 
     static async getById(data) {
         const result = await OrganizationDB.getById([data.region_id, data.id], data.isdeleted);
-        
+
+        return result;
+    }
+
+    static async import(data) {
+
+    }
+
+    static async readFile(data) {
+        const workbook = xlsx.readFile(data.filePath);
+        const sheetName = workbook.SheetNames[3];
+        const sheet = workbook.Sheets[sheetName];
+
+        const result = xlsx.utils.sheet_to_json(sheet).map(row => {
+            const newRow = {};
+
+            for (const key in row) {
+                if (Object.prototype.hasOwnProperty.call(row, key)) {
+                    newRow[key] = row[key];
+                }
+            }
+
+            return newRow;
+        });
+
         return result;
     }
 }
 
-            
+
