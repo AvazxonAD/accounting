7613d9935a781dbca3fcba06b65a02fa08f107ba
AvazxonAD <asqarovavazxon77@gmail.com>
2025-02-06 14:31:57 +0500

    jurnal

--- src/spravochnik/organization/service.js
+++ src/spravochnik/organization/service.js
@@ -8,7 +8,7 @@ const fs = require('fs').promises;
 
 exports.OrganizationService = class {
     static async templateFile() {
-        const fileName = `organization.template.xlsx`;
+        const fileName = `organization.xlsx`;
         const folderPath = path.join(__dirname, `../../../public/template`);
 
         const filePath = path.join(folderPath, fileName);
