8c7ee6e6a4206b95099a5afa9f3237b3bc293784
AvazxonAD <asqarovavazxon77@gmail.com>
2025-02-06 17:00:53 +0500

    jurnal

--- src/features/db.js
+++ src/features/db.js
@@ -2,13 +2,16 @@ const { db } = require('../db/index');
 
 exports.FeaturesDB = class {
     static async getDocNum(tableName, params) {
+        const main_schet_filter = tableName !== 'shartnomalar_organization' ? 'AND d.main_schet_id = $2' : '';
+        params = tableName !== 'shartnomalar_organization' ? params : params.splice(1, 1);
+        
         const query = `
             SELECT d.doc_num
             FROM ${tableName} d
             JOIN users u ON u.id = d.user_id
             JOIN regions r ON r.id = u.region_id
             WHERE r.id = $1
-              AND d.main_schet_id = $2
+              ${main_schet_filter}
               AND d.doc_num ~ '^[0-9]+$'
               AND d.isdeleted = false
             ORDER BY CAST(d.doc_num AS DOUBLE PRECISION) DESC
@@ -17,6 +20,6 @@ exports.FeaturesDB = class {
 
         const data = await db.query(query, params);
   
-        return data[0] || { doc_num: 1 };
+        return data[0] || { doc_num: 0 };
     }
 };
