1cdecd7f528ce6ece410f069e08a55176e7383df
AvazxonAD <asqarovavazxon77@gmail.com>
2025-02-11 21:30:32 +0500

    jurnal

---
+++ src/jur7/saldo/index.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+const router = Router();
+
+const { Controller } = require('./controller');
+const { validator } = require('../../helper/validator');
+
+const {
+    createSaldoSchema,
+    getSaldoSchema,
+    getSaldoRasxodSchema
+} = require("./schema");
+
+
+router.post('/', validator(Controller.createSaldo, createSaldoSchema))
+    .get('/', validator(Controller.getSaldo, getSaldoSchema))
+    .get('/rasxod', validator(Controller.getSaldoForRasxod, getSaldoRasxodSchema));
+
+module.exports = router;
