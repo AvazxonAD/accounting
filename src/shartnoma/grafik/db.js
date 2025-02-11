4d230c4640e5d4c10ab4d76df0b21c1a5b22b271
AvazxonAD <asqarovavazxon77@gmail.com>
2025-02-11 11:15:14 +0500

    jurnal

--- src/shartnoma/grafik/db.js
+++ src/shartnoma/grafik/db.js
@@ -4,7 +4,7 @@ exports.GrafikDB = class {
     static async deleteGrafikByContractId(params, client) {
         const _db = client || db;
 
-        const query = `UPDATE shartnoma_grafik SET ideleted = false WHERE id_shartnomalar_organization = $1 AND isdeleted = false`;
+        const query = `UPDATE shartnoma_grafik SET isdeleted = true WHERE id_shartnomalar_organization = $1 AND isdeleted = false`;
 
         await _db.query(query, params);
     }
