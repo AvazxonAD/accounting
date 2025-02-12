51cfa5c2da752acb39aa99c3f74f54ec841621e4
AvazxonAD <asqarovavazxon77@gmail.com>
2025-02-12 18:38:06 +0500

    jurnal

---
+++ eco.system.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+      {
+        name: "jurnal",
+        script: "node",
+        args: "-r module-alias/register src/server.js",
+        watch: false,
+        env: {
+          NODE_ENV: "production",
+        },
+      },
+    ],
+  };
+  
