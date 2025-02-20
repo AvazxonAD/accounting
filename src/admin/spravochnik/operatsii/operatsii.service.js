74125b33a89b33ba985d603bf3383c15469c4127
AvazxonAD <asqarovavazxon77@gmail.com>
2025-02-20 10:35:44 +0500

    jurnal

--- src/admin/spravochnik/operatsii/operatsii.service.js
+++ src/admin/spravochnik/operatsii/operatsii.service.js
@@ -91,6 +91,16 @@ const getAllOperatsiiService = async (offset, limit, type_schet, search, meta_se
         FROM spravochnik_operatsii s 
         LEFT JOIN spravochnik_budjet_name b ON b.id = s.budjet_id
         WHERE s.isdeleted = false 
+          (
+            s.type_schet = 'akt' OR 
+            s.type_schet = 'bank_prixod' OR 
+            s.type_schet = 'avans_otchet' OR 
+            s.type_schet = 'kassa_prixod' OR 
+            s.type_schet = 'kassa_rasxod' OR 
+            s.type_schet = 'bank_rasxod' OR 
+            s.type_schet = 'general' OR 
+            s.type_schet = 'show_service'
+          )
           ${schet_filter}
           ${sub_schet_filter}
           ${search_filter} 
