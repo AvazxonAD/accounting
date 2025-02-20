b4dc57949a9c9ed2190d46322fb4040d23024c9c
AvazxonAD <asqarovavazxon77@gmail.com>
2025-02-20 13:51:28 +0500

    upload 20.2.2025

--- src/region/jur7/rasxod/controller.js
+++ src/region/jur7/rasxod/controller.js
@@ -82,6 +82,7 @@ exports.Controller = class {
       return res.error(req.i18n.t('mainSchetNotFound'), 404);
     }
 
+    
     const oldData = await Jur7RsxodService.getById({ region_id, id, main_schet_id });
     if (!oldData) {
       return res.error(req.i18n.t('docNotFound'), 404);
