2d508e5285fc1f7d6304aae5f4daeebcb4d808db
AvazxonAD <asqarovavazxon77@gmail.com>
2025-03-06 15:26:08 +0500

    jurnal

--- src/region/jur7/prixod/controller.js
+++ src/region/jur7/prixod/controller.js
@@ -254,7 +254,7 @@ exports.Controller = class {
     }
 
     for (let child of childs) {
-      child.group = await GroupService.getById({ id: product.group.id });
+      child.group = await GroupService.getById({ id: child.group_jur7_id });
       if (!child.group) {
         return res.error(req.i18n.t('groupNotFound'), 404);
       }
