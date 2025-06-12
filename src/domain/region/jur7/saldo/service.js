56844db251fc0077d0919c3383a42378a0a6a4d2
AvazxonAD <asqarovavazxon77@gmail.com>
2025-06-12 17:38:05 +0500

    jurnal

--- src/domain/region/jur7/saldo/service.js
+++ src/domain/region/jur7/saldo/service.js
@@ -124,7 +124,7 @@ exports.Jur7SaldoService = class {
       budjet_id: data.budjet_id,
     });
 
-    result.forEach((item, index) => {
+    result.data.forEach((item, index) => {
       if (item.prixodData.length === 0) {
         item.prixodData.push({ doc_date: item.doc_date, doc_id: item.prixod_id, docId: item.prixod_id, doc_num: item.doc_num });
       }
