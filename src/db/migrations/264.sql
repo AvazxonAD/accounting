d450135e9e44bd7cf55c5d4e6d17d08f0f13db29
AvazxonAD <asqarovavazxon77@gmail.com>
2025-06-21 11:23:03 +0500

    jurnal

---
+++ src/db/migrations/264.sql
@@ -0,0 +1,41 @@
+UPDATE
+    spravochnik_podpis_dlya_doc
+SET
+    doljnost_name = 'Rahbar'
+WHERE
+    doljnost_name = 'rukovoditel';
+
+UPDATE
+    spravochnik_podpis_dlya_doc
+SET
+    doljnost_name = 'Bosh hisobchi'
+WHERE
+    doljnost_name = 'glav_buxgalter';
+
+UPDATE
+    spravochnik_podpis_dlya_doc
+SET
+    doljnost_name = 'MIB Boshlig''i'
+WHERE
+    doljnost_name = 'glav_mib';
+
+UPDATE
+    spravochnik_podpis_dlya_doc
+SET
+    doljnost_name = 'Bo''lim boshlig''i'
+WHERE
+    doljnost_name = 'nachalnik_otdela';
+
+UPDATE
+    spravochnik_podpis_dlya_doc
+SET
+    doljnost_name = 'Hisobchi'
+WHERE
+    doljnost_name = 'buxgalter';
+
+UPDATE
+    spravochnik_podpis_dlya_doc
+SET
+    doljnost_name = 'Kassir'
+WHERE
+    doljnost_name = 'kassir';
---
+++ src/db/migrations/265.sql
@@ -0,0 +1,41 @@
+UPDATE
+    spravochnik_podpis_dlya_doc
+SET
+    doljnost_name = 'Rahbar'
+WHERE
+    doljnost_name = 'RUKOVODITEL';
+
+UPDATE
+    spravochnik_podpis_dlya_doc
+SET
+    doljnost_name = 'Bosh hisobchi'
+WHERE
+    doljnost_name = 'GLAV_BUXGALTER';
+
+UPDATE
+    spravochnik_podpis_dlya_doc
+SET
+    doljnost_name = 'MIB Boshlig''i'
+WHERE
+    doljnost_name = 'GLAV_MIB';
+
+UPDATE
+    spravochnik_podpis_dlya_doc
+SET
+    doljnost_name = 'Bo''lim boshlig''i'
+WHERE
+    doljnost_name = 'NACHALNIK_OTDELA';
+
+UPDATE
+    spravochnik_podpis_dlya_doc
+SET
+    doljnost_name = 'Hisobchi'
+WHERE
+    doljnost_name = 'BUXGALTER';
+
+UPDATE
+    spravochnik_podpis_dlya_doc
+SET
+    doljnost_name = 'Kassir'
+WHERE
+    doljnost_name = 'KASSIR';
